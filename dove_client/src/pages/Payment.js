5af87170a659fd75022a2a14522c567a12bfd6f3
Chrecci <chrecci@gmail.com>
2023-02-24 19:18:54 +0800

    Test payments
    using locally stored mnemonics, add to keyring again, and then test transfer tokens. Also show more connected chain data

--- dove_client/src/pages/Payment.js
+++ dove_client/src/pages/Payment.js
@@ -9,9 +9,12 @@ import {
     mnemonicValidate,
     ed25519PairFromSecret
   } from '@polkadot/util-crypto';
-var { encodeAddress } = require('@polkadot/util-crypto')
+// var { encodeAddress } = require('@polkadot/util-crypto')
 import { isTestChain } from '@polkadot/util'
 import { set } from 'react-native-reanimated';
+const { decodeAddress, encodeAddress } = require('@polkadot/keyring');
+const { hexToU8a, isHex } = require('@polkadot/util');
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const TextBoxStyle = StyleSheet.create({
     input: {
@@ -46,13 +49,11 @@ function Payment({ navigation }) {
     
 
     async function connect() {
-        const keyring = new Keyring({ type: 'sr25519' });
-
         const api = await ApiPromise.create({ provider: wsProvider });
         const chainInfo = await api.registry.getChainProperties()
 
         console.log(chainInfo);
-
+        console.log("Native Token Name:", api.registry.chainTokens[0], "Plancks per Token: 10^", api.registry.chainDecimals[0])
         // Confirm genesis hash
         console.log(api.genesisHash.toHex());
         
@@ -72,64 +73,13 @@ function Payment({ navigation }) {
         console.log(
             `You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`
         );
-
-        // const createAccount = (mnemonic) => {
-        //     mnemonic = mnemonic && mnemonicValidate(mnemonic) 
-        //          ? mnemonic 
-        //          : mnemonicGenerate();
-        //     const account = keyring.addFromUri(mnemonic);
-        //     return { account, mnemonic };
-        // }
-        // console.log("CREAITING ACCOUNT", createAccount()); 
-        // add existing accounts to keyring through hard derivations https://polkadot.js.org/docs/keyring/start/suri
-        const rando = keyring.addFromUri('//5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY')
-        const rando1 = keyring.addFromUri('//5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy')
-
-        if (!sender) {
-            setSender(rando)
-        }
-        if (!recipient) {
-            setRecipient(rando1)
-        }
-        console.log('sender', sender)
-        console.log('recipient', recipient)
-        console.log(`${rando.meta.name}: has address ${rando.address} with publicKey [${rando.publicKey}]`);
-
-        const rando2 = keyring.addFromUri('//5GsYgNoJasVspbqftfmPRtcGqZ8UDnmwhu8HeAEkd2LhcV8S')
-        console.log("ALL PAIRS", keyring.getPairs(), keyring.getPairs().length)
-
-        // Adjust how many accounts to query at once.
-        let limit = 50;
-        let result = [];
-        let last_key = "";
+        return api
+    }
     
-        // while (true) {
-        //     let query = await api.query.system.account.entriesPaged({ args: [], pageSize: limit, startKey: last_key });
-        //     console.log("LENGTH", query.length)
-        //     if (query.length == 0) {
-        //         break
-        //     }
-    
-        //     for (const user of query) {
-        //         let account_id = encodeAddress(user[0].slice(-32));
-        //         let free_balance = user[1].data.free.toString();
-        //         let reserved_balance = user[1].data.reserved.toString();
-        //         result.push({ account_id, free_balance, reserved_balance });
-        //         last_key = user[0];
-        //     }
-        // }
-
-        // console.log(result)
-
-        
-        }
-    connect();  
     async function transfer() {
-        const api = await ApiPromise.create({ provider: wsProvider });
+        const api = await connect(); 
         const keyring = new Keyring({ type: 'sr25519' });
         const alice = keyring.addFromUri('//Alice')
-        console.log('sender transfer', alice)
-        console.log('recipient transfer', recipient)
         // const createAccount = (mnemonic) => {
         //     mnemonic = mnemonic && mnemonicValidate(mnemonic) 
         //          ? mnemonic 
@@ -140,6 +90,9 @@ function Payment({ navigation }) {
         // console.log("CREAITING ACCOUNT", createAccount()); 
         const test_user = keyring.addFromMnemonic('illness gossip weapon vast cable wet write depart angry used leaf leisure')
         const test_user1 = keyring.addFromMnemonic('walk cupboard parrot combine arena inquiry stereo talk sense maple action neutral')
+        AsyncStorage.getItem('mnemonic').then((data) => setRecipient(data))
+        console.log(recipient.slice(1, -1));
+        const user = keyring.addFromMnemonic(recipient.slice(1, -1))
         console.log("TEST TRANSFER PAIRS", keyring.getPairs(), keyring.getPairs().length)
         console.log("TEST USER ADDRESSES", test_user["address"], test_user1["address"])
         const unsub = await api.query.system.account.multi(['5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY', test_user["address"], test_user1["address"]], (balances) => {
@@ -147,7 +100,7 @@ function Payment({ navigation }) {
           
             console.log(`The balances are ${balance1.free.toHuman()} and ${balance2.free.toHuman()} and ${balance3.free.toHuman()}`);
           });
-        const transfer = api.tx.balances.transfer(test_user1["address"], 10);
+        const transfer = api.tx.balances.transfer(user["address"], 12345);
         const info = await api.tx.balances
         .transfer(test_user1["address"], 1001)
         .paymentInfo(test_user);
@@ -159,13 +112,13 @@ function Payment({ navigation }) {
         partialFee=${info.partialFee.toHuman()}
         `);
         // Sign and send the transaction using our account
-        const hash = await transfer.signAndSend(alice);
+        // const hash = await transfer.signAndSend(alice);
         
-        console.log('Transfer sent with hash', hash.toHex());
-        const unsub1 = await api.query.system.account.multi(['5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY', test_user["address"], test_user1["address"]], (balances) => {
-            const [{ data: balance1 }, { data: balance2 }, { data: balance3 }] = balances;
+        // console.log('Transfer sent with hash', hash.toHex());
+        const unsub1 = await api.query.system.account.multi(['5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY', test_user["address"], test_user1["address"], user["address"]], (balances) => {
+            const [{ data: balance1 }, { data: balance2 }, { data: balance3 }, { data: balance4 }] = balances;
           
-            console.log(`The new balances are ${balance1.free.toHuman()} and ${balance2.free.toHuman()} and ${balance3.free.toHuman()}`);
+            console.log(`The new balances are ${balance1.free.toHuman()} and ${balance2.free.toHuman()} and ${balance3.free.toHuman()} and ${balance4.free.toHuman()}`);
           });
         
           // Adjust how many accounts to query at once.
@@ -193,6 +146,22 @@ function Payment({ navigation }) {
 
     }
     
+
+    const isValidAddressPolkadotAddress = (address) => {
+        try {
+            encodeAddress(
+            isHex(address)
+                ? hexToU8a(address)
+                : decodeAddress(address)
+            );
+
+            return true;
+        } catch (error) {
+            return false;
+        }
+    };
+
+    const isValid = isValidAddressPolkadotAddress();
     const onSubmit = () => {
         return navigation.navigate('Profile')
     }
@@ -218,7 +187,7 @@ function Payment({ navigation }) {
                     keyboardType="numeric"
                 />
                 <Pressable
-                    onPress={recipient ? transfer : onSubmit}
+                    onPress={transfer}
                     style={SubmitButtonStyle.input}
                 >
                     <Text>Submit Transaction</Text>
